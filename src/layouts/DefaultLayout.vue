a1ad738e30d270b5a4e6f9ea759ed6961807db64
Zaccheaus <zaccheausyisa00@gmail.com>
2023-02-26 15:09:50 -0800

    Design: add default layot for all pages

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="py-2 max-w-5xl">
+    <NavBar />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import AppHero from '../components/AppHero.vue'
-import NavBar from '../components/NavBar.vue'
 import ProductsDisplay from '../components/ProductsDisplay.vue'
 </script>
 
 <template>
-  <div class="flex flex-col items-center py-2 max-w-5xl">
-    <NavBar />
+  <div class="flex flex-col items-center">
     <AppHero />
     <ProductsDisplay />
   </div>
