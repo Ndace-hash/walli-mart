9585bc360c8690250da755d2afca6cfbc2c920a3
Zaccheaus <zaccheausyisa00@gmail.com>
2023-03-04 00:37:57 -0800

    chores: postioning footer on cart view

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-2 max-w-5xl relative">
     <NavBar />
-    <div class="mb-12">
+    <div class="mb-12 min-h-full">
       <slot />
     </div>
     <AppFooter class="absolute bottom-0" />
