d40fc3386d6e8927813ee19ecc10b9692cbf7ecb
Zaccheaus <zaccheausyisa00@gmail.com>
2023-03-10 20:44:22 -0800

    design: made hero component responsive

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="hero relative w-full grid grid-cols-2 gap-4 pl-4">
-    <div class="flex flex-col gap-y-2 justify-center">
+  <section class="hero relative flex items-center w-full md:grid grid-cols-2 gap-4 px-4">
+    <div
+      class="flex flex-col gap-y-2 justify-center text-center items-center md:items-start text-left"
+    >
       <h1 class="text-3xl font-bold capitalize leading-tight mb-2">
         Get Your Hands on the Latest Must-Haves with Our Online Shopping Destination!
       </h1>
@@ -13,7 +15,7 @@
         <span>buy now</span> <Font-awesome-icon icon="fa-solid fa-tag" />
       </button>
     </div>
-    <div class="h-full flex place-items-center overflow-y-hidden object-cover">
+    <div class="h-full flex place-items-center overflow-y-hidden object-cover hidden md:flex">
       <img src="../assets/display.jpg" class="w-full h-full" />
     </div>
   </section>
